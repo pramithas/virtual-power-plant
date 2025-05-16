41e0530385a25f47bcd1ed370df4afe6da9fe545
pramithas <dhakalpramithas@gmail.com>
2025-05-16 10:40:01 +0545

    Initial commit

---
+++ src/test/java/com/example/vpp/TestVppApplication.java
@@ -0,0 +1,11 @@
+package com.example.vpp;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestVppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(VppApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
